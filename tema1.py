77451cd5018bbce859bb6bdd88e55e663301ce43
daria-medeleanu <medeleanudaria14@gmail.com>
2025-03-04 14:03:02 +0200

    first-ex

---
+++ tema1.py
@@ -0,0 +1,18 @@
+#ex1
+def find_machine_epsilon():
+    m = 1
+    u = 10 ** -m
+
+    while (1.0 + u) != 1.0:  
+        m += 1
+        u = 10 ** -m
+
+    u = 10 ** -(m)
+    print(f"Precizia masina este: {u}, iar m este {m}")
+
+    if (1.0 + 10 ** -(m+1) == 1.0):
+        print("Verificare trecuta cu succes")
+    else: 
+        print("Verificare esuata")
+
+find_machine_epsilon()
