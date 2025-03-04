dca85627da8cd5dd7978513c924c580233d405da
daria-medeleanu <medeleanudaria14@gmail.com>
2025-03-04 23:10:15 +0200

    modified-parameters-3

--- tema1.py
+++ tema1.py
@@ -1,10 +1,8 @@
-#ex1
 from functools import reduce
 import math
 import random
 import struct
 import time
-
 
 u = 0
 def setU(value):
@@ -30,7 +28,7 @@ def find_machine_epsilon():
     else: 
         print("Verificare esuata")
     setU(u)
-
+print("ex1")
 find_machine_epsilon()
 
 def addition_nonassociativity():
@@ -42,45 +40,61 @@ def addition_nonassociativity():
         print("Adunarea nu este asociativa:)")
     else: 
         print("Adunarea este asociativa:()")
+    print(f"(x+y)+z={(x+y)+z}")
+    print(f"x+(y+z)={x+(y+z)}")
+print("ex2a")
 addition_nonassociativity()
 
+def float2bin(f):
+    #pack the float f as a double precision binary big endian (most significant bit first) string
+    packed = struct.pack('!d', f)
+    #unpack the double precision binary string as an unsigned long long
+    #[0] the first and only element of the tuple returned by struct.unpack
+    unpacked = struct.unpack('!Q', packed)[0]
+    return f'{unpacked:064b}'
 
 def random_numbers():
     return random.uniform(0, 1),random.uniform(0, 1), random.uniform(0, 1)
 def good_example():
-    #contains representable numbers
+    #contains one representable number
+    print('0.5 = ', float2bin(0.5))
+    print('0.1 = ', float2bin(0.1))
+    print('0.2 = ', float2bin(0.2))
     return 0.5, 0.1, 0.2
 def bad_example():
     #contains unrepresentable numbers
+    print('0.75 = ', float2bin(0.75))
+    print('0.2 = ', float2bin(0.2))
+    print('0.3 = ', float2bin(0.3))
     return 0.1, 0.2, 0.3
-def float2bin(f):
-    # [d] = struct.unpack(">Q", struct.pack(">d", f))
-    # return f'{d:064b}'
-    bits = struct.unpack('!Q', struct.pack('!d', f))[0]
-    return f'{bits:064b}'
+
     
 def multiplication_nonassociativity():
     
     x, y, z = good_example()
-    print(float2bin(x))
-    print(float2bin(y))
     a, b, c = bad_example()
 
     print(f"x = {x}, y = {y}, z= {z}" )
+    print(f"(x*y)*z={(x*y)*z}")
+    print(f"x*(y*z)={x*(y*z)}")
     if (x*y)*z!=x*(y*z):
         print("Inmultirea nu este asociativa:)")
     else: 
         print("Inmultirea este asociativa:()")
     print(f"a = {a}, b = {b}, c= {c}" )
+    print(f"(a*b)*c={(a*b)*c}")
+    print(f"a*(b*c)={a*(b*c)}")
     if (a*b)*c!=a*(b*c):
         print("Inmultirea nu este asociativa:)")
     else: 
         print("Inmultirea este asociativa:()")
-
+print("ex2b")
 multiplication_nonassociativity()
 
-def horner_evaluate(coefficients, x):
-    return reduce(lambda acc, coef: acc * x + coef, coefficients, 0)
+def horner_evaluate(coeff, value):
+  if len(coeff) == 1:
+    return coeff[0]
+  return value*horner_evaluate(coeff[1:], value)+coeff[0]
 
 def sinus():
 
@@ -92,7 +106,6 @@ def sinus():
     # def P6(x, c3, c4) : return x*(1 + x**2*(-0.16666 + x**2*(0.0083333 + x**2*(-c3 + x**2*c4))))
     # def P7(x, c1, c2, c3, c4, c5) : return x*(1 + x**2*(-c1 + x**2*(c2 + x**2*(-c3 + x**2*(c4 + x**2*(-c5))))))
     # def P8(x, c1, c2, c3, c4, c5, c6) : return x*(1 + x**2*(-c1 + x**2*(c2 + x**2*(-c3 + x**2*(c4 + x**2*(-c5+ x**2*c6))))))
-
 
     c1 = 0.16666666666666666666666666666667
     c2 = 0.00833333333333333333333333333333
@@ -110,10 +123,9 @@ def sinus():
     timep8 = 0
 
     best_polynomials = [0] * 9
-    for i in range(10000):
+    for _ in range(10000):
         nr = random.uniform(-math.pi/2, math.pi/2)
         val_sin_exact = math.sin(nr)
-        
         
         # p1 = P1(nr, c1, c2)
         # p2 = P2(nr, c1, c2, c3)
@@ -124,55 +136,55 @@ def sinus():
         # p7 = P7(nr, c1, c2, c3, c4, c5)
         # p8 = P8(nr, c1, c2, c3, c4, c5, c6)
     
-        p1 = horner_evaluate([1, 0, -1, 0, 1], nr)
-        p2 = horner_evaluate([1, 0, -1, 0, 1, 0, -1], nr)
-        p3 = horner_evaluate([1, 0, -1, 0, 1, 0, -1, 0, 1], nr)
-        p4 = horner_evaluate([1, 0, -0.166, 0, 0.00833, 0, -1, 0, 1], nr)
-        p5 = horner_evaluate([1, 0, -0.1666, 0, 0.008333, 0, -1, 0, 1], nr)
-        p6 = horner_evaluate([1, 0, -0.16666, 0, 0.0083333, 0, -1, 0, 1], nr)
-        p7 = horner_evaluate([1, 0, -1, 0, 1, 0, -1, 0, 1, 0, -1], nr)
-        p8 = horner_evaluate([1, 0, -1, 0, 1, 0, -1, 0, 1, 0, -1, 0, 1], nr)
+        p1 = horner_evaluate([1, 0, -c1, 0, c2], nr)
+        p2 = horner_evaluate([1, 0, -c1, 0, c2, 0, -c3], nr)
+        p3 = horner_evaluate([1, 0, -c1, 0, c2, 0, -c3, 0, c4], nr)
+        p4 = horner_evaluate([1, 0, -0.166, 0, 0.00833, 0, -c3, 0, c4], nr)
+        p5 = horner_evaluate([1, 0, -0.1666, 0, 0.008333, 0, -c3, 0, c4], nr)
+        p6 = horner_evaluate([1, 0, -0.16666, 0, 0.0083333, 0, -c3, 0, c4], nr)
+        p7 = horner_evaluate([1, 0, -c1, 0, c2, 0, -c3, 0, c4, 0, -c5], nr)
+        p8 = horner_evaluate([1, 0, -c1, 0, c2, 0, -c3, 0, c4, 0, -c5, 0, c6], nr)
 
 
         errors = []
-        start_time = time.time()
+        start_time = time.time_ns()
         errorp1 = abs(p1 - val_sin_exact)
-        timep1 += time.time()- start_time
+        timep1 += time.time_ns()- start_time
         errors.append((1, errorp1))
         
-        start_time = time.time()
+        start_time = time.time_ns()
         errorp2 = abs(p2 - val_sin_exact)
-        timep2 += time.time()- start_time
+        timep2 += time.time_ns()- start_time
         errors.append((2, errorp2))
 
-        start_time = time.time()
+        start_time = time.time_ns()
         errorp3 = abs(p3 - val_sin_exact)
-        timep3 += time.time()- start_time
+        timep3 += time.time_ns()- start_time
         errors.append((3, errorp3))
 
-        start_time = time.time()
+        start_time = time.time_ns()
         errorp4 = abs(p4 - val_sin_exact)
-        timep4 += time.time()- start_time
+        timep4 += time.time_ns()- start_time
         errors.append((4, errorp4))
 
-        start_time = time.time()
+        start_time = time.time_ns()
         errorp5 = abs(p5 - val_sin_exact)
-        timep5 += time.time()- start_time
+        timep5 += time.time_ns()- start_time
         errors.append((5, errorp5))
         
-        start_time = time.time()
+        start_time = time.time_ns()
         errorp6 = abs(p6 - val_sin_exact)
-        timep6 += time.time()- start_time
+        timep6 += time.time_ns()- start_time
         errors.append((6, errorp6))
         
-        start_time = time.time()
+        start_time = time.time_ns()
         errorp7 = abs(p7 - val_sin_exact)
-        timep7 += time.time()- start_time
+        timep7 += time.time_ns()- start_time
         errors.append((7, errorp7))
         
-        start_time = time.time()
+        start_time = time.time_ns()
         errorp8 = abs(p8 - val_sin_exact)
-        timep8 += time.time()- start_time
+        timep8 += time.time_ns()- start_time
         errors.append((8, errorp8))
 
         errors.sort(key = lambda x : x[1])
@@ -189,4 +201,5 @@ def sinus():
     best_time_polynomials.sort(key = lambda x : x[1])
     print(best_time_polynomials)
 
+print("ex3")
 sinus()
